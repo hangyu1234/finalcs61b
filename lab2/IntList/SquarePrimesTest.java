92b249b174ccb58a62b21167633c20a5871c70c6
hangyu1234 <yuhang1234@sjtu.edu.cn>
2025-12-01 22:48:29 +0800

    Intlist

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,12 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+
+    @Test
+    public void testSquarePrimeSimple2() {
+        IntList lst = IntList.of( 17, 15, 17);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("289 -> 15 -> 289", lst.toString());
+        assertTrue(changed);
+    }
 }
